f5bbe372a9269f6dd49d37e218fbfa9156a28d9e
pow <poooow@gmail.com>
2024-06-06 18:01:57 +0200

    Show sidebar on load with param

--- src/hooks/useCurrentMarker.tsx
+++ src/hooks/useCurrentMarker.tsx
@@ -1,9 +1,10 @@
 import { useDataContext } from "@/context/Data"
 import { useEffect } from "react"
 import { useSearchParams } from 'react-router-dom'
+
 export default function useCurrentMarker() {
   const [searchParams, setSearchParams] = useSearchParams()
-  const { currentMarkerId, setCurrentMarkerId } = useDataContext()
+  const { currentMarkerId, setCurrentMarkerId, setShowSidebar } = useDataContext()
 
   const setParamId = (id: number) => {
     setSearchParams({ 'id': id.toString() })
@@ -21,6 +22,7 @@ export default function useCurrentMarker() {
   const setId = (id: number) => {
     setCurrentMarkerId(id)
     setParamId(id)
+    setShowSidebar(true)
   }
 
   return { setId, getId }
