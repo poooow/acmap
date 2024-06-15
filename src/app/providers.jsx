abcec0cacb4c5a26a04e294c5489ceecc8c611b1
pow <poooow@gmail.com>
2024-06-15 10:31:49 +0200

    Prevent router server rendering

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,9 +1,12 @@
-"use client"
-
-import DataProvider from "../context/Data"
-import { BrowserRouter } from "react-router-dom"
+'use client'
+import DataProvider from "../context/Data";
+import { BrowserRouter } from "react-router-dom";
 
 export function Providers({ children }) {
+  // Prevent router ssr
+  if (typeof window === "undefined")
+    return <DataProvider>{children}</DataProvider>;
+
   return (
     <BrowserRouter>
       <DataProvider>{children}</DataProvider>
